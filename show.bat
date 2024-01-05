f641159028b3b4ab4a5af134d1373d33c4622a1b
yoshdownload <155772931+yoshdownload@users.noreply.github.com>
2024-01-06 03:11:54 +0700

    Update show.bat

--- show.bat
+++ show.bat
@@ -1,25 +1,4 @@
 
- 1
- 2
- 3
- 4
- 5
- 6
- 7
- 8
- 9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
 @echo off
 python -c "import pyautogui as pag; pag.click(785, 17, duration=2)"
 python -c "import pyautogui as pag; pag.click(903, 64, duration=2)"
